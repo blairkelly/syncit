ffa8370ee1e683fa5c2ae27b33897171e5644dfc
Blair Kelly <blairkelly1@gmail.com>
2014-01-23 13:50:01 -0500

    small tweaks

--- client.js
+++ client.js
@@ -1,3 +1,6 @@
+var partnerlocation = "10.0.1.8";
+var partnerport = 2008;
+
 var locationhostname = window.location.hostname;
 var serverip = locationhostname;
 
@@ -6,6 +9,14 @@ var serverport = locationport;
 
 var socket = io.connect('//'+serverip+':'+serverport);
 socket.on('welcome', function(data) {
+    console.log(data.message);
+    console.log('Handshake address: ' + data.address);
+});
+
+
+var socket_remote = io.connect('//'+partnerlocation+':'+partnerport);
+socket_remote.on('welcome', function(data) {
+    console.log("Connected remote");
     console.log(data.message);
     console.log('Handshake address: ' + data.address);
 });
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //needed for init.
 var config = {}
 
-config.partnerlocation = "10.0.1.16";
+config.partnerlocation = "10.0.1.8";
 config.partnerport = 2008;
 config.localport = 3000;
 config.filepath = "../target/";
